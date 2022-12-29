d6d11903bfbd86b196592b9202a2c24366ba88fb
Baptiste Hugot <19981614+BaptisteHugot@users.noreply.github.com>
2022-12-29 19:09:25 +0100

    Correction of a typo

--- carte_arret_cuivre.js
+++ carte_arret_cuivre.js
@@ -18,7 +18,7 @@ var info;
 function getColor(year) {
   return year == "31 mars 2021" ? '#1B9E77' :
   year == "31 mars 2023" ? '#D95F02' :
-  year == "janvier 2025" ? '#7570B3' :
+  year == "Janvier 2025" ? '#7570B3' :
   '#D8B2D8';
 }
 
@@ -163,7 +163,7 @@ function initMap() {
   legend.onAdd = function(map) {
     var div = L.DomUtil.create('div', 'info legend'),
     grades = [],
-    labels = ["31 mars 2021", "31 mars 2023", "janvier 2025"];
+    labels = ["31 mars 2021", "31 mars 2023", "Janvier 2025"];
 
     // On boucle sur toutes les valeurs et on génère une étiquette avec la bonne couleur pour chaque valeur
     for (var i = 0; i < labels.length; i++) {
