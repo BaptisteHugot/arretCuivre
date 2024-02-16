e138d2b495b8f7663babe156f5bceeae5ab00526
Baptiste Hugot <19981614+BaptisteHugot@users.noreply.github.com>
2024-02-17 00:34:59 +0100

    Ajout fermeture commerciale
    Ajout de la fermeture commerciale, et des dernières annonces de fermeture technique

--- carte_arret_cuivre.js
+++ carte_arret_cuivre.js
@@ -1,5 +1,5 @@
 /**
-* Fichier générant la carte qui sera affichée montrant les communes concernées par l'arrêt du cuivre, par département et par date d'arrêt
+* Fichier générant la carte qui sera affichée montrant les communes concernées par l'arrêt du cuivre, par date d'arrêt
 */
 
 // Initialisation des variables
@@ -8,20 +8,29 @@ var longitude = 2.602778;
 var map = null;
 var GeoSearchControl = window.GeoSearch.GeoSearchControl;
 var OpenStreetMapProvider = window.GeoSearch.OpenStreetMapProvider;
-var geojsonLayer;
+var fermetureTechniqueLayer;
+var fermetureCommercialeLayer;
 var info;
+var control;
 
 /**
 * Fonction définissant la couleur qui est affectée à une année particulière
 * @param L'année concernée
 */
 function getColor(year) {
-  return year == "31 mars 2021" ? '#1B9E77' :
-  year == "31 mars 2023" ? '#D95F02' :
-  year == "31 janvier 2025" ? '#7570B3' :
-  year == "31 mars 2025" ? '#E7298A' :
-  year == "27 janvier 2026" ? '#66A61E' :
-  '#D8B2D8';
+  return year == "19 novembre 2020" ? "#1b9e77" :
+  year == "31 mars 2021" ? '#1B9E77' :
+  year == "31 mars 2022" ? "#d95f02"  :
+  year == "31 mars 2023" ? '#d95f02' :
+  year == "31 janvier 2024" ? "#7570b3" :
+  year == "31 mars 2024" ? "#e7298a" : 
+  year == "27 janvier 2025" ? "#66a61e" :
+  year == "31 janvier 2025" ? '#7570b3' :
+  year == "31 mars 2025" ? '#e7298a' :
+  year == "27 janvier 2026" ? '#66a61e' :
+  year == "31 janvier 2026" ? "#e6ab02" : 
+  year == "31 janvier 2027" ? "#e6ab02" : 
+  '#d8b2d8';
 }
 
 /**
@@ -65,7 +74,8 @@ function highlightFeature(e) {
 * @param Évènement
 */
 function resetHighlight(e) {
-  geojsonLayer.resetStyle(e.target);
+  fermetureTechniqueLayer.resetStyle(e.target);
+  fermetureCommercialeLayer.resetStyle(e.target);
   info.update();
 }
 
@@ -85,9 +95,18 @@ function zoomToFeature(e) {
 function onEachFeature(feature, layer) {
   layer.on({
     mouseover: highlightFeature,
-    mouseout: resetHighlight,
+    mouseout: resetHighlight
     //click: zoomToFeature
   });
+}
+
+/**
+* Fonction qui déplace des éléments au format HTML dans un nouveau parent
+* @param Eléments à déplacer
+* @param Le nouvel élément où l'élément "el" sera déplacé
+*/
+function setParent(el, newParent) {
+  newParent.appendChild(el);
 }
 
 /**
@@ -97,9 +116,11 @@ function initMap() {
   // Créer l'objet "map" et l'insèrer dans l'élément HTML qui a l'ID "map"
   map = L.map('map', {
     fullscreenControl: {
-      pseudoFullscreen: false // if true, fullscreen to page width and height
+      pseudoFullscreen: false, // if true, fullscreen to page width and height
+      position: 'topleft'
     }
   }).setView([latitude, longitude], 6);
+  
   // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
   var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
@@ -111,13 +132,29 @@ function initMap() {
   var hash = new L.Hash(map);
 
   // On lit les données contenues dans le fichier geojson
-  geojsonLayer = new L.GeoJSON.AJAX("./Donnees/carte_arret_cuivre.json", {
+  fermetureTechniqueLayer = new L.GeoJSON.AJAX("./Donnees/fermeture_technique.json", {
+    style: style,
+    onEachFeature: onEachFeature
+  });
+  fermetureCommercialeLayer = new L.GeoJSON.AJAX("./Donnees/fermeture_commerciale.json", {
     style: style,
     onEachFeature: onEachFeature
   });
 
-  // On définit la légende de la carte
-  var legend = L.control({
+  var overlayMaps = {
+    "Fermeture technique": fermetureTechniqueLayer,
+    "Fermeture commerciale": fermetureCommercialeLayer 
+  };
+
+  control = L.control.layers(overlayMaps, null, {
+    collapsed: false
+  }); // On ajoute les couches précédentes à la carte, en les rendant exclusives
+
+  // On définit les 2 légendes de la carte
+  var legendFermetureTechnique = L.control({
+    position: 'bottomright'
+  });
+  var legendFermetureCommerciale = L.control({
     position: 'bottomright'
   });
 
@@ -159,17 +196,33 @@ function initMap() {
   });
 
   /**
-  * On ajoute la légende à la carte
+  * On ajoute la légende pour la fermeture technique à la carte
   * @param la carte où la légende sera ajoutée
   */
-  legend.onAdd = function(map) {
+  legendFermetureTechnique.onAdd = function(map) {
     var div = L.DomUtil.create('div', 'info legend'),
     grades = [],
-    labels = ["31 mars 2021", "31 mars 2023", "31 janvier 2025", "31 mars 2025", "27 janvier 2026"];
+    labelsFermetureTechnique = ["31 mars 2021", "31 mars 2023", "31 janvier 2025", "31 mars 2025", "27 janvier 2026", "31 janvier 2027"];
 
     // On boucle sur toutes les valeurs et on génère une étiquette avec la bonne couleur pour chaque valeur
-    for (var i = 0; i < labels.length; i++) {
-      div.innerHTML += '<i style="background:' + getColor(labels[i]) + '"></i> ' + labels[i] + '<br>';
+    for (var i = 0; i < labelsFermetureTechnique.length; i++) {
+      div.innerHTML += '<i style="background:' + getColor(labelsFermetureTechnique[i]) + '"></i> ' + labelsFermetureTechnique[i] + '<br>';
+    }
+    return div;
+  };
+
+  /**
+  * On ajoute la légende pour la fermeture commerciale à la carte
+  * @param la carte où la légende sera ajoutée
+  */
+  legendFermetureCommerciale.onAdd = function(map) {
+    var div = L.DomUtil.create('div', 'info legend'),
+    grades = [],
+    labelsFermetureCommerciale = ["19 novembre 2020", "31 mars 2022", "31 janvier 2024", "31 mars 2024", "27 janvier 2025", "31 janvier 2026"];
+
+    // On boucle sur toutes les valeurs et on génère une étiquette avec la bonne couleur pour chaque valeur
+    for (var i = 0; i < labelsFermetureCommerciale.length; i++) {
+      div.innerHTML += '<i style="background:' + getColor(labelsFermetureCommerciale[i]) + '"></i> ' + labelsFermetureCommerciale[i] + '<br>';
     }
     return div;
   };
@@ -177,7 +230,7 @@ function initMap() {
   info = L.control();
 
   /**
-  * On ajoute les informatios à la carte
+  * On ajoute les informations à la carte
   * @param la carte où les informations seront ajoutées
   */
   info.onAdd = function(map) {
@@ -192,7 +245,7 @@ function initMap() {
   */
   info.update = function(props) {
     this._div.innerHTML = '<h4>Date arrêt Cuivre</h4>' + (props ?
-      '<b>' + props.data_arret_cuivre_DEP + '</b><br />' + props.data_arret_cuivre_DATE : '<br /><br />');
+      '<b>' + props.data_arret_cuivre_DATE + '</b>' : '<br /><br />');
     };
 
     // On définit l'export de la carte au format .png
@@ -216,14 +269,41 @@ function initMap() {
     });
 
     // On ajoute toutes les couches à la carte
+    control.addTo(map);
     osmLayer.addTo(map);
-    geojsonLayer.addTo(map);
-    legend.addTo(map);
+    fermetureTechniqueLayer.addTo(map);
+    legendFermetureTechnique.addTo(map);
     map.addControl(searchControl);
     location.addTo(map);
     info.addTo(map);
     exporter.addTo(map);
     printer.addTo(map);
+
+    // On met les boutons radio servant à contrôler la carte qui sera affichée en dehors de la carte
+    var htmlObject = control.getContainer();
+    var a = document.getElementById('control');
+    setParent(htmlObject, a);
+
+     // On change l'endroit où s'affiche les boutons radio lorsque l'utilisateur passe en plein écran
+    map.on('fullscreenchange', function() {
+      if (map.isFullscreen()) {
+        control.addTo(map);
+      } else {
+        htmlObject = control.getContainer();
+        setParent(htmlObject, a);
+      }
+    });
+
+    // On change la légende de la carte en fonction des données affichées
+    map.on("baselayerchange", function(eventLayer){
+      if(eventLayer.name == "Fermeture technique"){
+        map.removeControl(legendFermetureCommerciale);
+        legendFermetureTechnique.addTo(map);
+      } else if(eventLayer.name == "Fermeture commerciale"){
+        map.removeControl(legendFermetureTechnique);
+        legendFermetureCommerciale.addTo(map);
+      }
+    });
 
   }
 
